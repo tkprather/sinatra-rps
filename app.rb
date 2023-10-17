0ddbe5a668a479dad9dbf435618d66221471f888
tkprather <146144703+tkprather@users.noreply.github.com>
2023-10-17 08:32:23 +0000

    hh

--- app.rb
+++ app.rb
@@ -13,6 +13,47 @@ get("/") do
       </p>
 
       <h1>Welcome to Rock-Paper-Scissors!</h1>
+
+
+<p>
+  Rock-paper-scissors (also known as paper, scissors, stone or other variants) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand.
+</p>
+
+<p>
+  These shapes are:
+</p>
+
+<ul>
+  <li>
+    rock (a closed fist)
+  </li>
+
+  <li>
+    paper (a flat hand)
+  </li>
+
+  <li>
+    scissors (a fist with the index and middle fingers extended, forming a V)
+  </li>
+
+</ul>
+
+<p>
+  A player who decides to play rock will beat another player who has
+  chosen scissors (rock crushes scissors or sometimes blunts
+  scissors), but will lose to one who has played paper (paper covers
+  rock); a play of paper will lose to a play of scissors (scissors
+  cut[s] paper). If both players choose the same shape, the game is tied
+  and is usually immediately replayed to break the tie.
+</p>
+
+<p>
+  Originating from China and Japan, other names for the game in the
+  English-speaking world include roshambo and other orderings of the three
+  items, with rock sometimes being called stone.
+</p>
+
+
   "
 end
 
